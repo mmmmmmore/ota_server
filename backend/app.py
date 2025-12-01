4f59d9ff0b30680e92e1abec3f7305aab4e5649c
mmmmmmore <maochuneric@gmail.com>
2025-12-01 09:58:21 +0800

    sync app.py

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from routes.devices import devices_bp
 from routes.software import software_bp
 from routes.upload import upload_bp
 from routes.dispatch import dispatch_bp
+from routes.download import download_bp
 
 app = Flask(__name__)
 CORS(app)  # 解决跨域问题，前端不同源也能访问
@@ -15,6 +16,7 @@ app.register_blueprint(devices_bp)
 app.register_blueprint(software_bp)
 app.register_blueprint(upload_bp)
 app.register_blueprint(dispatch_bp)
+app.register_blueprint(download_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
