59b2ababb4b70dfa5a87f5fc48c911faf976bc1a
mmmmmmore <maochuneric@gmail.com>
2025-12-01 09:56:55 +0800

    Update dispatch.py

--- backend/routes/dispatch.py
+++ backend/routes/dispatch.py
@@ -12,25 +12,30 @@ os.makedirs(TASK_DIR, exist_ok=True)
 
 GW_IP = "192.168.4.1"
 GW_PORT = 9000  # 假设网关监听端口9000
+OTA_Ser_IP = "192.168.4.2"  # server IP
 
 
 def create_task_file(device_name, client_id, version):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = f"{timestamp}_{device_name}_{client_id}.json"
+    task_id = f"{timestamp}_{client_id}"
+    filename = f"{task_id}.json"
     filepath = os.path.join(TASK_DIR, filename)
-    
+
     task = {
+        "task_id": task_id,
         "device_name": device_name,
         "client_id": client_id,
         "version": version,
+        "firmware_url": f"http://{OTA_Ser_IP}:8080/firmware/firmware_{version}.bin",
         "timestamp": timestamp,
         "status": "pending"
     }
-    
+
     with open(filepath, "w") as f:
         json.dump(task, f, indent=2)
-    
+
     return filepath, task
+
 
 
 def is_gateway_online():
@@ -44,7 +49,6 @@ def is_gateway_online():
 
 
 
-
 @dispatch_bp.route("/api/dispatch/push", methods=["POST"])
 def push_task():
     data = request.get_json()
@@ -52,26 +56,23 @@ def push_task():
     client_id = data.get("client_id")
     version = data.get("version")
 
-    #if not device_name or not client_id or not version:
-    #    return jsonify({"error": "缺少必要字段"}), 400
-
     filepath, task = create_task_file(device_name, client_id, version)
 
-    # 检查网关是否在线
     if not is_gateway_online():
-        update_task_status(filepath, task, "failed", "Network not connect to GW, please retry connection... ")
-        return jsonify({"error", "Network Connection Err"}), 503
+        update_task_status(filepath, task, "failed", "GW not reachable")
+        return jsonify({"error": "Network Connection Err"}), 503
 
-    # 推送任务到网关
     try:
         sock = socket.create_connection((GW_IP, GW_PORT), timeout=5)
         sock.sendall(json.dumps(task).encode("utf-8"))
+        response = sock.recv(1024).decode("utf-8")
         sock.close()
-        update_task_status(filepath, task, "success")
-        return jsonify({"message": "OTA Push Success ", "task": task}), 200
+        update_task_status(filepath, task, "success", response)
+        return jsonify({"message": "OTA Push Success", "task": task, "gw_response": response}), 200
     except Exception as e:
         update_task_status(filepath, task, "failed", str(e))
         return jsonify({"error": f"Push Err: {str(e)}"}), 500
+
 
 
 
@@ -108,6 +109,7 @@ def get_stats():
         data["percent"] = round(success / total * 100, 2) if total > 0 else 0
 
     return jsonify(stats)
+
 
 
 
