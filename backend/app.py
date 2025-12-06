3ca8b97975f71135b4b02cb9701afe46db680d8f
mmmmmmore <maochuneric@gmail.com>
2025-12-06 09:49:40 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+import asyncio, threading
+
 
 
 # 导入蓝图
 from routes.devices import devices_bp
 from routes.software import software_bp
 from routes.upload import upload_bp
-from routes.dispatch import dispatch_bp
+from routes.dispatch import dispatch_bp, task_queue, loop
 from routes.download import download_bp
-from routes.dispatch import tcp_client
+from routes.tcp_async import GatewayClient
+
+GW_IP = "192.168.4.1"
+GW_TCP_PORT = 9001
 
 app = Flask(__name__)
 CORS(app, resources={r"/*":{"origins":"*"}},supports_credentials=True)  # 解决跨域问题，前端不同源也能访问 # config the cert
 
 
-int_done = False
 
 # 注册蓝图
 app.register_blueprint(devices_bp)
@@ -25,9 +29,17 @@ app.register_blueprint(download_bp)
 
 
 
+def start_asyncio():
+    global loop, task_queue
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    task_queue = asyncio.Queue()
+    client = GatewayClient(GW_IP, GW_TCP_PORT, task_queue)
+    loop.run_until_complete(client.run())
 
+threading.Thread(target= start_asyncio, daemon=True ).start()
 
 if __name__ == "__main__":
-    tcp_client.manager_thread.start()
     context = ("server.crt","server.key")
     app.run(host="0.0.0.0", port=8080, debug=True,  ssl_context = context)
+
