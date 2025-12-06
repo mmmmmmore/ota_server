17ec3e88f6677016b4a4ebf401fb8d9aeebbccba
mmmmmmore <maochuneric@gmail.com>
2025-12-06 13:02:55 +0800

    251206-130000 change download GET path to firmware folder

--- backend/routes/download.py
+++ backend/routes/download.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, send_from_directory
-
+import os
 download_bp = Blueprint("download", __name__)
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FIRMWARE = os.path.join(BASE_DIR, "..", "firmware")
+
 
 @download_bp.route("/firmware/<filename>", methods=["GET"])
 def download_firmware(filename):
     # 这里的路径是后端实际存放固件的目录
-    return send_from_directory("../firmware", filename)
+    return send_from_directory(FIRMWARE, filename)
