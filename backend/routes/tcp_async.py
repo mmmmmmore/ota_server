55aacfe657a1fbc6b2caa6a8bbf27b0a4866e798
mmmmmmore <maochuneric@gmail.com>
2025-12-07 14:12:06 +0800

    251207-140000 add hello in tcp service

--- backend/routes/tcp_async.py
+++ backend/routes/tcp_async.py
@@ -9,8 +9,14 @@ class GatewayClient:
     async def run(self):
         while True:
             try:
+                
+                hellp_msg ={"msg_type":"hello", "role":"ota_server"}
                 reader, writer = await asyncio.open_connection(self.ip, self.port)
                 print("[TCP] Connected to GW")
+                hello_payload = json.dumps(hellp_msg) +'\n'
+                writer.write((json.dumps(hellp_msg)+'\n').encode())
+                await writer.drain()
+                print("[TCP] Tx hello to OTA GW finished")
 
                 async def sender():
                     while True:
