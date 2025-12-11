b7d6d5d3b83bb84cab2588acc57d71c7bfed44ae
mmmmmmore <maochuneric@gmail.com>
2025-12-11 18:00:14 +0800

    log sync

--- backend/routes/tcp_async.py
+++ backend/routes/tcp_async.py
@@ -14,7 +14,7 @@ class GatewayClient:
                 hellp_msg ={"msg_type":"hello", "role":"ota_server"}
                 reader, writer = await asyncio.open_connection(self.ip, self.port)
                 print("[TCP] Connected to GW")
-                hello_payload = json.dumps(hellp_msg) +'\n'
+                #hello_payload = json.dumps(hellp_msg) +'\n'
                 writer.write((json.dumps(hellp_msg)+'\n').encode())
                 await writer.drain()
                 print("[TCP] Tx hello to OTA GW finished")
